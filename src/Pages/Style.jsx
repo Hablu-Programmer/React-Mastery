9d081f56dce8d6a10d85c5c00d5ab96a44b090bc
Hablu-Programmer <habluprogrammer@gmail.com>
2022-12-26 15:07:23 +0600

    modules

--- src/Pages/Style.jsx
+++ src/Pages/Style.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import  "./Style.css"
+import HabluStyles from "./hablu.module.css";
 
 function Style() {
     const ElementOne = {
-        color:"red",
+        color:"White",
         backgroundColor:"green",
         textAlign:"center"
     };
   return (
     <div>
         <h1 style={ElementOne}>my first style component</h1>
+        <h2 className='Error'>welcome to hablu programmer</h2>
+        <h2 className='Error'>welcome to hablu programmer</h2>
+        <h2 className='Error'>welcome to hablu programmer</h2>
+        <h2 className={HabluStyles.Error}>welcome to hablu programmer</h2>
+        <button className={HabluStyles.eshan}>Click Me</button>
     </div>
   )
 }
