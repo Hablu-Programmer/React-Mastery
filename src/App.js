a11f291f158f4691f717c832c32381619e85ac04
Hablu-Programmer <habluprogrammer@gmail.com>
2022-12-27 12:24:26 +0600

    form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import ReactBootstrap from './Pages/ReactBootstrap';
+import Input from './Pages/Form/Input';
 
 function App() {
   return (
     <React.Fragment >
       <ReactBootstrap / >
-        
+        <Input />
     </React.Fragment>
   );
 }
